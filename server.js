ff2e03668c501544a194e0917364909f61e9ec7f
rcp440 <rcp440@gmail.com>
2026-05-15 12:53:05 -0300

    Fix CORS: permitir header x-superadmin-token

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ allowedHeaders: ['Content-Type', 'x-superadmin-token'] }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
